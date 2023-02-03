a7064b867bb466a5ea2ff93b20301dd78e0df7ed
masoud arabi <applepie@masouds-MacBook-Air.local>
2023-02-03 00:25:39 -0500

    add post

---
+++ test/models/post_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  test 'should not be save whthout a title' do 
+    post = Post.new 
+    assert post.save
+  end
+  test 'should be save with a title' do 
+    post = Post.new(title: 'first title')
+    assert post.save
+  end
+end
